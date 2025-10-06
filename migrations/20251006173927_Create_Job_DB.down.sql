f7ab298634c53d9c625f51ef74d6635c568e41ce
daniel dencker <djepse20@student.aau.dk>
2025-10-07 00:15:45 +0200

    added db migrations

--- migrations/20251006173927_Create_Job_DB.down.sql
+++ migrations/20251006173927_Create_Job_DB.down.sql
@@ -1,1 +1,7 @@
 -- Add down migration script here
+DROP TABLE location_for_job;
+DROP TABLE tags_for_job;
+DROP TABLE job_location;
+DROP TABLE job_tag;
+DROP TABLE job;
+DROP TABLE company_info;
--- migrations/20251006173927_Create_Job_DB.up.sql
+++ migrations/20251006173927_Create_Job_DB.up.sql
@@ -1,5 +1,58 @@
 -- Add up migration script here
-CREATE TABLE job_tag (
-    id BIGINT PRIMARY KEY,
-    job_tag VARCHAR(255)
+CREATE TABLE company_info (
+    id bigint PRIMARY KEY,
+    name varchar(255),
+    logo_url varchar(1000)
 );
+
+CREATE Table job (
+    id bigserial PRIMARY KEY,
+    title varchar(255),
+    description text,
+    job_url text,
+    company_id bigint,
+    CONSTRAINT company_info_fk FOREIGN KEY (company_id)
+    REFERENCES company_info(id)
+
+);
+
+CREATE TABLE job_tag (
+    id bigserial PRIMARY KEY,
+    tag varchar(255)
+);
+
+
+CREATE TABLE tags_for_job (
+    job_id bigint,
+    job_tag_id bigint,
+    CONSTRAINT job_fk Foreign KEY (job_id) 
+    REFERENCES job(id),
+    CONSTRAINT job_tag_fk Foreign KEY (job_tag_id)  
+    REFERENCES job_tag(id),
+
+    CONSTRAINT tags_for_job_pk PRIMARY KEY (job_id,job_tag_id)
+
+);
+
+
+CREATE TABLE job_location (
+    id bigserial PRIMARY KEY,
+    name varchar(255),
+    geo_location point
+
+);
+CREATE TABLE location_for_job (
+    job_id bigint,
+    location_id bigint,
+    CONSTRAINT job_fk Foreign KEY (job_id) REFERENCES job(id),
+    CONSTRAINT location_fk Foreign KEY (location_id)  REFERENCES job_location(id),
+
+    CONSTRAINT location_for_job_pk PRIMARY KEY (job_id,location_id)
+
+);
+
+
+
+
+
+
